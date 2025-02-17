377e52e26b6397d3ee62f3760a23e8085f857f7c
zh <zhanghang@Ditto>
2025-02-17 17:16:17 +0800

    task2

--- src/primer/trie_store.cpp
+++ src/primer/trie_store.cpp
@@ -21,13 +21,14 @@ namespace bustub {
  */
 template <class T>
 auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<T>> {
-  // Pseudo-code:
-  // (1) Take the root lock, get the root, and release the root lock. Don't lookup the value in the
-  //     trie while holding the root lock.
-  // (2) Lookup the value in the trie.
-  // (3) If the value is found, return a ValueGuard object that holds a reference to the value and the
-  //     root. Otherwise, return std::nullopt.
-  throw NotImplementedException("TrieStore::Get is not implemented.");
+  std::lock_guard<std::mutex> root_lock(root_lock_);
+  Trie trie = root_;
+  auto val = trie.Get<T>(key);
+  if (val != nullptr) {
+    return ValueGuard<T>(trie, *val);
+  } else {
+    return std::nullopt;
+  }
 }
 
 /**
@@ -36,16 +37,18 @@ auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<T>> {
  */
 template <class T>
 void TrieStore::Put(std::string_view key, T value) {
-  // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
-  // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Put is not implemented.");
+  std::lock_guard<std::mutex> write_lock(write_lock_);
+  Trie new_trie = root_.Put<T>(key, std::move(value));
+  std::lock_guard<std::mutex> root_lock(root_lock_);
+  root_ = new_trie;
 }
 
 /** @brief This function will remove the key-value pair from the trie. */
 void TrieStore::Remove(std::string_view key) {
-  // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
-  // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Remove is not implemented.");
+  std::lock_guard<std::mutex> write_lock(write_lock_);
+  Trie new_trie = root_.Remove(key);
+  std::lock_guard<std::mutex> root_lock(root_lock_);
+  root_ = new_trie;
 }
 
 // Below are explicit instantiation of template functions.
